0160901d4446d2f164f757f05d839affc5a7af44
Austin Crane <austincrane@gmail.com>
2016-08-08 12:45:11 -0400

    fixed slider size

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $size = 'full';
 	            <li> 
 	              
 	                <a href="<?php echo $image['url']; ?>">
-	                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+	                     <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
 	                </a>
 	                
 	            </li>
